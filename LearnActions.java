c3f7a84fb27b7fb0aa92e8b34480977d4ee56dba
Lokesh Kumar <100150334+lokeshnani43@users.noreply.github.com>
2022-03-24 23:04:53 +0530

    Add files via upload

---
+++ LearnActions.java
@@ -0,0 +1,31 @@
+package week4.day2;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LearnActions {
+
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions option = new ChromeOptions();
+		option.addArguments("--disable-notification");
+		ChromeDriver driver = new ChromeDriver(option);
+		driver.get("https://ajio.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+		WebElement men = driver.findElement(By.xpath("//a[text()]"));
+		Actions build = new Actions(driver);
+		build.moveToElement(men).contextClick().perform();
+		
+	}
+
+}
---
+++ LearnDragandDrop.java
@@ -0,0 +1,34 @@
+package week4.day2;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LearnDragandDrop {
+
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.leafground.com/pages/drop.html");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+		WebElement drag = driver.findElement(By.id("draggable"));
+		WebElement drop = driver.findElement(By.id("droppable"));
+
+		Actions build = new Actions(driver);
+			
+		build.dragAndDrop(drag, drop).perform();
+		
+		
+	}
+
+}
